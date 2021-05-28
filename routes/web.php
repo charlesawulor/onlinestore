de94b2d11a593c6b3e88e84e7d46c39647fbd686
Charles Awulor <c.awulor@yahoo.com>
2021-05-28 10:37:10 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,52 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+   
 });
+*/
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+/*
+Route::get('/hello', function () {
+ 
+    return '<h1>Hello world</h1>';
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+*/
+Route::get('/users/{id}', function ($id) {
+    
+    return 'This is user ' .$id;
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts','PostsController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
+
+Route::get('login/google', 'Auth\LoginController@redirectToProvider1');
+Route::get('login/google/callback', 'Auth\LoginController@handleProviderCallback1');
+
+
+
+
