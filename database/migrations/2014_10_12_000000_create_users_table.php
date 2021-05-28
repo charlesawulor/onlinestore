92c54e243c0934fa5cadd34e090253a08671ff44
Charles Awulor <c.awulor@yahoo.com>
2021-05-28 10:48:39 +0100

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->string('provider_id')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
